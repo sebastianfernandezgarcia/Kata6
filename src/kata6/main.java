cf8b3acfc49fb31b05a98c8eab7e2f6e561cf32f
Sebastián Fernández <sebasfernandezgar@gmail.com>
2021-12-27 22:41:40 +0000

    Version 2

--- src/kata6/main.java
+++ src/kata6/main.java
@@ -4,25 +4,48 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import toys.Car;
+import toys.Helicopter;
 import toys.SerialNumberGenerator;
 public class main {
     
     public static void main(String[] args) {
         SerialNumberGenerator generator = new SerialNumberGenerator();
         ArrayList<Car> cars = new ArrayList<>();
+        
+        ArrayList<Helicopter> helicopters= new ArrayList<>();
 
        Scanner in = new Scanner(System.in);
        String line = "";
        while(!line.equals("exit")) {
            line = in.nextLine();
-           if (!line.equals("exit")) {
-               Car car = new Car(generator.next());
-               car.pack();
-               car.label();
-               cars.add(car);
-               System.out.println("Built cars: "+cars.stream()
-                                            .map(c -> c.getSerialNumber().toString())
-                                               .collect(Collectors.joining(", ")));
+           
+           switch (line) {
+               case "car": 
+                    Car car = new Car(generator.next());
+                    car.pack();
+                    car.label();
+                    cars.add(car);
+                    System.out.println("Built cars: "+cars.stream()
+                                                 .map(c -> c.getSerialNumber().toString())
+                                                    .collect(Collectors.joining(", ")));
+                    break;
+                    
+                case "helicopter": 
+                    Helicopter helicopter = new Helicopter(generator.next());
+                    helicopter.pack();
+                    helicopter.label();
+                    helicopters.add(helicopter);
+                    System.out.println("Built helicopters: "+helicopters.stream()
+                                                 .map(c -> c.getSerialNumber().toString())
+                                                    .collect(Collectors.joining(", ")));
+                    break;
+                    
+                case "exit":
+                    System.out.println("Exiting...");
+                    break;
+                    
+                default:
+                    System.out.println("Command unknown!");
               
            }
        }
--- src/toys/Helicopter.java
+++ src/toys/Helicopter.java
@@ -1,12 +1,12 @@
 package toys;
 
 
-public class Car {
+public class Helicopter {
     final private Integer serialNumber;
     
-    final String type = "car";
+    final String type = "helicopter";
     
-    public Car(Integer serialNumber) {
+    public Helicopter(Integer serialNumber) {
         this.serialNumber = serialNumber;
     }
     
